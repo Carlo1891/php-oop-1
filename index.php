82d11cba4bfb9e871e858c40671cea3e380b5fc1
Carlo1891 <carlo.18@live.it>
2022-09-02 16:39:30 +0200

    feat: Add movie function get

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+    include __DIR__ . '/class/movies.php';
+
 ?>
 
 <!DOCTYPE html>
@@ -9,11 +11,16 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PHP e il cinema</title>
+        <link rel="stylesheet" href="./css/style.css">
     </head>
     <body>
         <head>
             <h1>Films sul grande schermo:</h1>
         </head>
+
+        <main>
+            lista
+        </main>
         
     </body>
 </html>
