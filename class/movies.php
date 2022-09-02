02f38b97f0174f56096da237ddf76de11b031f1f
Carlo1891 <carlo.18@live.it>
2022-09-02 16:39:59 +0200

    Add class

--- class/movies.php
+++ class/movies.php
@@ -1,1 +1,23 @@
 <!-- Films da includere nell'index -->
+
+<?php 
+    // classe film
+    class Movie{
+        private $title;
+        private $year;
+        // immesse variabili
+        {
+            function __construct( $_title, $_year){
+                $this -> title = $_title;
+                $this -> year = $_year;
+            }
+
+            public function getTitle(){
+                return $this -> title;
+            }
+            public function getTitle(){
+                return $this -> year;
+            }
+    }
+
+?>
